6b4c478c263ffa0e71bba331f6b70aa17ee02d89
Gabriel Banevicius <gcbanevicius@gmail.com>
2013-07-10 10:28:50 +0200

    first commit!

---
+++ config/initializers/refinery/i18n.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+Refinery::I18n.configure do |config|
+  # config.enabled = true
+
+  # config.default_locale = :en
+
+  # config.current_locale = :en
+
+  # config.default_frontend_locale = :en
+
+  # config.frontend_locales = [:en]
+
+  # config.locales = {:el=>"Ελληνικά", :it=>"Italiano", :rs=>"Srpski", :nl=>"Nederlands", :de=>"Deutsch", :da=>"Dansk", :lv=>"Latviski", :fr=>"Français", :sk=>"Slovenský", :nb=>"Norsk Bokmål", :ru=>"Русский", :en=>"English", :ja=>"日本語", :sl=>"Slovenian", :"zh-CN"=>"Simplified Chinese", :sv=>"Svenska", :bg=>"Български", :cs=>"Česky", :es=>"Español", :"zh-TW"=>"Traditional Chinese", :pl=>"Polski", :"pt-BR"=>"Português"}
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PaideiaRefinery::Application.config.secret_token = 'd4ba162f6afeef4459dda7d500527375a03f3fc8824030bd0caa45a41cb8e34a9f88bd07f13f53a85cc33825ff2e846ffc825496cfd0fcc44fc2d557f5eadced'
